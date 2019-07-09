690ccec5f190fc104d5a9bfcc42b2458882e22c1
beyondverage0908 <beyondaverage0908@gmail.com>
2019-07-09 12:41:20 +0800

    使用storage进行数据管理

--- src/js/pipe.js
+++ src/js/pipe.js
@@ -2,27 +2,29 @@
 	root['pipe'] = factory();
 })(this, function() {
 
-	let _config = null;
-	let _callback_monitor = null;
 	let _is_preview = false;
 	let _pagewidth = 750;
 	let _ratio = 1;
+	let _storage = null;
 
 	function Storage(configuration) {
 		this.__config = configuration;
 		this.__listener = null;
 		this.__monitor = null;
 
+		this.all = function() {
+			return this.__config || {};
+		}
 		this.get = function(key) {
 			return this.__config[key];
 		}
 		this.save = function(key, value, origin) {
 			this.__config[key] = value;
 
-			if (this.__listener && origin === 'configer') {
+			if (this.__listener && origin === 'drager') {
 				this.__listener(key, value);
 			}
-			if (this.__monitor && origin === 'drager') {
+			if (this.__monitor && origin === 'configer') {
 				this.__monitor(key, value);
 			}
 		}
@@ -40,7 +42,6 @@
 		}
 	}
 	
-
 	function addScript(path) {
 		$('<script>', {
 			src: path
@@ -52,6 +53,78 @@
 			rel: 'stylesheet',
 			href: path
 		}).appendTo('head');
+	}
+
+	function postPassport(loginName, verifyCode) {
+		// 进行ajax请求
+		return new Promise((resolve, reject) => {
+			if (!loginName || !verifyCode) {
+				alert('未输入电话号码或验证码')
+				reject();
+				return;
+			}
+			resolve();
+		})
+	}
+
+	function renderPassport(target, nodeEl) {
+		let htmlTemplate = `
+			<div style="position: fixed; top: 0; left: 0; width: ${_pagewidth}px; height: 100%; z-index: 100; background-color: rgba(0, 0, 0, 0.7); display: flex; justify-content: center;">
+				<div style="position: absolute; top: ${_ratio * 200}px; background-color: #F8F9FB; padding: ${_ratio * 10}px ${_ratio * 20}px ${_ratio * 20}px ${_ratio * 20}px;">
+					<div style="border-bottom: solid 1px #F4F5F7; padding: ${_ratio * 30}px 0px;">
+						<span style="font-size: ${_ratio * 30}px; color: #333333;">+86</span>
+						<input id="mobile-number-id" type="text" placeholder="请输入手机号码" style="border-style: none; box-sizing: border-box; outline: none; background-color: #F8F9FB; padding: ${_ratio * 10}px 5px; font-size: ${_ratio * 30}px; width: 85%; color: #333333;" />
+					</div>
+					<div style="border-bottom: solid 1px #F4F5F7; padding: ${_ratio * 30}px 0px;">
+						<span style="font-size: ${_ratio * 28}px; color: #333333;">验证码</span>
+						<input id="mobile-verifycode-id" type="password" placeholder="请输入验证码" style="border-style: none; box-sizing: border-box; outline: none; background-color: #F8F9FB; padding: ${_ratio * 10}px ${_ratio * 5}px; border-right: solid 1px #F4F5F7; font-size: ${_ratio * 30}px; color: #333333;">
+						<span id="btn-verifycode-id" style="color: #F9C1A4; font-size: ${_ratio * 28}px; display: inline-block; padding: 0 ${_ratio * 10}px; text-align: center; cursor: pointer;">发送验证码</span>
+					</div>
+					<div style="font-size: ${_ratio * 28}px; color: #C1C1C3; text-align: center; margin: ${_ratio * 35}px auto ${_ratio * 40}px auto;">未注册的手机号码验证后将自动创建东方财富账号</div>
+					<div id="passport-login-container" style="text-align: center;">
+					</div>
+				</div>
+			</div>
+		`;
+		$($.parseHTML(htmlTemplate)).appendTo('#the-current-page');
+
+		// 验证码获取
+		$(document).on('click', '#btn-verifycode-id', function() {
+			alert('获取验证码');
+		});
+
+		const passport = _storage.get('passport');
+		if (passport && passport.image) {
+			$('<img>', {
+				src: passport.image,
+				click: () => {
+					const loginMobile = $('#mobile-number-id').val();
+					const loginVerifyCode = $('#mobile-verifycode-id').val();
+					postPassport(loginMobile, loginVerifyCode).then(res => {
+						if (target === 'web') {
+							window.location.href = nodeEl.url;
+						} else if (target === 'app') {
+							window.location.href = `sechem.url?url=${nodeEl.url}`;
+						}
+					}).catch(err => {})
+				}
+			}).appendTo('#passport-login-container');
+		} else {
+			const defualtLoginBtn = `<div id="passport-default-login" style="color: #ffffff; background-color: #FFA17C; border-radius: 5px; text-align: center; font-size: ${_ratio * 30}px; height: ${_ratio * 80}px; line-height: ${80 * _ratio}px; cursor: pointer;">登录</div>`; 
+			$($.parseHTML(defualtLoginBtn)).appendTo('#passport-login-container');
+
+			$(document).on('click', '#passport-default-login', function() {
+				const loginMobile = $('#mobile-number-id').val();
+				const loginVerifyCode = $('#mobile-verifycode-id').val();
+				postPassport(loginMobile, loginVerifyCode).then(res => {
+					if (target === 'web') {
+						window.location.href = nodeEl.url;
+					} else if (target === 'app') {
+						window.location.href = `sechem.url?url=${nodeEl.url}`;
+					}
+				}).catch(err => {})
+			})
+		}
 	}
 
 	function createEl(el) {
@@ -77,13 +150,18 @@
 			const props = {};
 			if (_is_preview) {
 				props.click = () => {
-					if (el.action === 'app') {
-						alert(el.url);
-					} else if (el.action === 'href') {
-						alert('href');
+					// 四种组合
+					if (el.target === 'web' && el.action === 'href') {
+						// 直接跳转
 						window.location.href = el.url;
-					} else if (el.action === 'passport') {
-						alert('passport');
+					} else if (el.target === 'web' && el.action === 'passport') {
+						// 登录信息收集框后进行跳转
+						renderPassport('web', el);
+					} else if (el.target === 'app' && el.action === 'href') {
+						// 在app中打开相应的url地址 -- 在target为app时候，需要提供download url
+					} else if (el.target === 'app' && el.action === 'passport') {
+						// 登录信息收集页面点击登录后 -- 打开app并进行再app中打开相应的页面
+						renderPassport('app', el);
 					}
 				}
 			} else {
@@ -220,19 +298,21 @@
 		})
 	}
 
-	function render(config, pagewidth, isPreview) {
-		_config = config;
+	function render(pagewidth, isPreview) {
+		const configuration = _storage.all();
 		_is_preview = isPreview;
 		_pagewidth = pagewidth;
-		_ratio = _pagewidth / (config.pagewidth || 750); // 确定页面布局比率
+		_ratio = _pagewidth / (configuration.pagewidth || 750); // 确定页面布局比率
 		setViewPort();
 		setTopContainer();
 
-		if (config.hasOwnProperty('topBanner') || config.hasOwnProperty('bottomBanner')) {
+		if (configuration.hasOwnProperty('topBanner') || configuration.hasOwnProperty('bottomBanner')) {
 			addScript('./src/libs/swiper/swiper.animate1.0.3.min.js');
 			addScript('./src/libs/swiper/swiper.js');
 			addCss('./src/libs/swiper/animate.min.css');
 			addCss('./src/libs/swiper/swiper.css');
+
+			setupSwiper();
 		}
 
 		// 配置平台
@@ -240,31 +320,29 @@
 			addScript('./src/js/dragula.js');
 			addScript('./src/js/displace.js');
 		}
-		for (let k in config) {
-			renderNode(k, config[k])
+		for (let k in configuration) {
+			renderNode(k, configuration[k])
 		}
 	}
 	// render(_config, false);
 
 	function setupSwiper() {
-		console.log(getQueryString('kk'));
-		const flag = getQueryString('kk');
 		// 需要先判断是否topBanners是否存在，且数据大于1
-		if (_config && _config['topBanner'] && _config['topBanner'].length) {
+		if (_storage && _storage.all() && _storage.get('topBanner') && _storage.get('topBanner').length) {
 			const options = {
 				autoplay: true,
 			};
-			if (_config['topBanner'].length > 1) {
+			if (_storage.get('topBanner').length > 1) {
 				options.loop = true;
 			}
 			new Swiper('#the-top-banners', options);
 		}
 		// 需要先判断是否bottomBanners是否存在，且数据大于1
-		if (_config && _config['bottomBanner'] && _config['bottomBanner'].length) {
+		if (_storage && _storage.all() && _storage.get('bottomBanner') && _storage.get('bottomBanner').length) {
 			const options = {
 				autoplay: true,
 			};
-			if (_config['bottomBanner'].length > 1) {
+			if (_storage.get('bottomBanner').length > 1) {
 				options.loop = true;
 			}
 			new Swiper('#the-bottom-banners', options);
@@ -280,9 +358,9 @@
 
 	function buttonsDisplace() {
 		const displace = window.displacejs;
-		if (_config && _config['buttons'] && _config['buttons'].length) {
+		if (_storage && _storage.all() && _storage.get('buttons') && _storage.get('buttons').length) {
 			const btns = [];
-			for (let i = 0; i < _config['buttons'].length; i++) {
+			for (let i = 0; i < _storage.get('buttons').length; i++) {
 				btns.push('.btn__' + i);
 			}
 			btns.map(cls => {
@@ -302,7 +380,7 @@
 								left: _pagewidth - el.clientWidth
 							})
 						}
-						if (_config && _config['topBanner'] && _config['topBanner']) {
+						if (_storage.all() && _storage.get('topBanner') && _storage.get('topBanner').length) {
 							const topHeight = $('#the-top-banners').height();
 							if ($(el).offset().top < topHeight) {
 								$(el).css({
@@ -310,7 +388,7 @@
 								})	
 							}
 						}
-						if (_config && _config['bottomBanner'] && _config['bottomBanner']) {
+						if (_storage.all() && _storage.get('bottomBanner') && _storage.get('bottomBanner')) {
 							const bottomHeight = $('#the-bottom-banners').height();
 							const contentHeight = $('#the-current-page').height();
 							const maxOffsetTop = contentHeight - bottomHeight - el.clientHeight;
@@ -327,12 +405,12 @@
 							const tempList = findCls.split('__');
 							const btnIndex = tempList[tempList.length - 1];
 
-							const btn = _config.buttons[btnIndex];
+							const btn = _storage.get('buttons')[btnIndex];
 							btn.position = {
 								top: el.offsetTop,
 								left: el.offsetLeft
 							}
-							pipe.pageEmit('buttons', _config['buttons']);
+							pageEmit('buttons', _storage.get('buttons'));
 						}
 					}
 				})
@@ -350,22 +428,21 @@
 			},
 			release: function(e) { 
 				// 监听鼠标谈起mouseup事件
-				if (_config && _config['contents'] && _config['contents'].length) {
+				if (_storage.all() && _storage.get('contents') && _storage.get('contents').length) {
 					const newContents = [];
 					const contents = $('#the-contents').children('img');
 					let isSame = true;
 					for (let i = 0; i < contents.length; i++) {
 						const imgIndex = $(contents[i]).attr('data-index');
-						newContents.push(_config.contents[imgIndex]);
+						newContents.push(_storage.get('contents')[imgIndex]);
 
-						if (_config.contents[i].image !== _config.contents[imgIndex].image) {
+						if (_storage.get('contents')[i].image !== _storage.get('contents')[imgIndex].image) {
 							isSame = false;
 						}
 					}
 					// 对比原有数据
 					if (!isSame) {
-						_config.contents = newContents;
-						pipe.pageEmit('contents', newContents);
+						pageEmit('contents', newContents);
 						renderNode('contents', newContents); // 更新element img标签的data-index属性
 					}
 				}
@@ -374,32 +451,26 @@
 	}
 
 	$(document).ready(function() {
-		setupSwiper();
-
-		// 非预览是才可以进行拖拽配置
-		if (!_is_preview) {
-			buttonsDisplace();
-			contentsDisplace();
-		}
+		// setupSwiper();
 	})
 
 	window.onload = function() {
 		// 视图矫正
 		// 1. 计算top banner和bottom banner的高度
-		if (_config && _config['topBanner'] && _config['topBanner']) {
+		if (_storage && _storage.all() && _storage.get('topBanner') && _storage.get('topBanner').length) {
 			const topHeight = $('#the-top-banners').height();
 			$('#the-contents').css({
 				'margin-top': topHeight
 			})
 		}
-		if (_config && _config['bottomBanner'] && _config['bottomBanner']) {
+		if (_storage && _storage.all() && _storage.get('bottomBanner') && _storage.get('bottomBanner')) {
 			const bottomHeight = $('#the-bottom-banners').height();
 			$('#the-contents').css({
 				'margin-bottom': bottomHeight
 			})
 		}
 		// 计算出按钮图片适配后的长宽
-		if (_config && _config['buttons'] && _config['buttons'].length) {
+		if (_storage && _storage.all() && _storage.get('buttons') && _storage.get('buttons').length) {
 			const buttons = $('#the-buttons').children();
 			for (let i = 0; i < buttons.length; i++) {
 				const btnWidth = $($(buttons[i]).children()[0]).width();
@@ -412,44 +483,23 @@
 
 	// 拖拽平台修改通知到配置平台
 	function pageEmit(key, obj) {
-		if (_callback_monitor) {
-			// 利用json stringify进行深拷贝 - 场景是由于config对象来源于json字符串
-			const copyObj = JSON.parse(JSON.stringify(obj));
-			_callback_monitor(key, copyObj);
-		}
-	}
-	// 接受配置平台的消息
-	function configListener(key, obj) {
-		renderNode(key, obj);
+		// 利用json stringify进行深拷贝 - 场景是由于config对象来源于json字符串
+		const copyObj = JSON.parse(JSON.stringify(obj));
+		_storage.save(key, copyObj, 'drager');
 	}
 
-	// 配置平台的改动需要通知到我这边进行数据结构的修改
-	function configChange(_key, _obj) {
-		_config[_key] = _obj;
-		if (configListener && typeof configListener === 'function') {
-			configListener(_key, _obj);
-		}
-	}
 
-	function monitor(fn) {
-		if (fn && typeof fn === 'function') {
-			_callback_monitor = fn;
-		}	
-	}
-
+	// 初始化storage
 	const factory = {};
 
 	function init(config) {
-		let storage = new Storage(config);
-		factory['storage'] = storage;
-		storage.monitor(function (key, value) {
-			console.log(key, value);
+		_storage = new Storage(config);
+		factory['storage'] = _storage;
+		// 监听配置平台的数据发生了改变，配置平台通过对storage对象的save(key, value)方法进行变更数据
+		_storage.monitor(function (key, value) {
+			renderNode(key, value);
 		})
 	}
-
-	factory['pageEmit'] = pageEmit;
-	factory['configChange'] = configChange;
-	factory['monitor'] = monitor;
 	factory['renderDom'] = render;
 	factory['init'] = init;
 
